d25676f8ce2b2962bda5b9df2bd3080f16dd193c
Alexey Makarov <alexeynkrv@gmail.com>
2021-03-30 17:04:40 +0700

    Initial Commit

---
+++ Homework_9_iOS_Skillbox/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework_9_iOS_Skillbox
+//
+//  Created by Alexey Makarov on 30.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Homework_9_iOS_SkillboxTests/Homework_9_iOS_SkillboxTests.swift
@@ -0,0 +1,33 @@
+//
+//  Homework_9_iOS_SkillboxTests.swift
+//  Homework_9_iOS_SkillboxTests
+//
+//  Created by Alexey Makarov on 30.03.2021.
+//
+
+import XCTest
+@testable import Homework_9_iOS_Skillbox
+
+class Homework_9_iOS_SkillboxTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
